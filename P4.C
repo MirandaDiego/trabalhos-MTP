857241e7b6c7876a624130b8ed978df6b9ccb8b8
MirandaDiego <37850304+MirandaDiego@users.noreply.github.com>
2018-06-24 22:13:48 -0300

    Update P4.C

--- P4.C
+++ P4.C
@@ -5,23 +5,23 @@ int  main ()
     int i = 0 ;
 	int cumulativa = 0 ;
 	bits de char [ 256 ];  
-	scanf ( " % s " , bits);
+	scanf ( " %s " , bits);
 	getchar ();
 
 	while (bits [i])
 	{
 
-		if (bits [i]> = 48 && bits [i] <= 57 ) // verifica se o caractere atual é um algarismo
+		if (bits [i]> = 48 && bits [i] <= 57 ) // verifica se o caractere atual Ã© um algarismo
 		{
 			bits [i] = bits [i] - ' 0 ' ; // converte char para int
 
-			cumulativa = cumulativa * 10 + bits [i]; // compõe o numero usando cada algarismo
+			cumulativa = cumulativa * 10 + bits [i]; // compÃµe o numero usando cada algarismo
 
 		}
 
 		i ++;
 	}
-	printf ( " \ n numero digitado e:% d " , cumulativa);
+	printf ( " \n numero digitado e:%d " , cumulativa);
 	getch ();
 
 	return  0 ;
