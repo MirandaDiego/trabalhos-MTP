5f9ff96e033d12a4ceff741c101a10b84caed860
MirandaDiego <37850304+MirandaDiego@users.noreply.github.com>
2018-06-24 21:55:24 -0300

    Add files via upload

---
+++ P6.C
@@ -0,0 +1,30 @@
+#include < stdio.h >
+
+int  soma ( int * pt, int * pz)
+{	
+	retorno (* pz) + ((pz == pt)? 0 : soma (pt, pz- 1 ));
+}
+
+int  main ()
+
+{
+	int tam;
+
+	printf ( " digite os numeros: \ n " );
+	scanf ( " % i " , & tam);	
+	getchar ();
+
+	int i, val [tam];
+	printf ( " digite os valores: \ n " );
+	para (i = 0 ; i <tam; i ++)
+
+	{
+	   scanf ( " % i " e val [i]);	
+       getchar ();
+	}
+
+	int * primeiro = val, * last = val + tam- 1 ;
+	printf ( " \ n media =% f \ n " , ( float ) soma (primeiro, ultimo) / tam);
+
+	return  0 ;
+}
