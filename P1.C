50ee4dde5f643a1cf299bc4d85d9604779507a74
MirandaDiego <37850304+MirandaDiego@users.noreply.github.com>
2018-06-27 20:18:26 -0300

    Update P1.C

--- P1.C
+++ P1.C
@@ -1,97 +1,53 @@
 #include <stdio.h>
 
-
 int main ()
-
 {
-
-	int estado = 0,i=0;
-
+	int est = 0, i = 0;
 	char bits[256];
-
-	scanf("%s" , &bits[i]);
-
+	scanf("%s", &bits[i]);
 	while (bits[i]!= '\0')
-
 	{
-
-		if (estado == 0 && bits [i] == '0')
-
+		if (est == 0 && bits [i] == '0')
 		{
-
-			estado = 0;
-
+			est = 0;
 		}
-
-		else if (estado == 0 && bits [i] == '1')
-
+		else if (est == 0 && bits [i] == '1')
 		{
-
-			estado = 1;
-
+			est = 1;	
 		}
-
-		else if (estado == 1 && bits[i] == '0')
-
+		else if (est == 1 && bits [i] == '0')
 		{
-
-			estado = 2;
-
+			est = 2;	
 		}
-
-		else if (estado == 2 && bits [i]=='1')
-
+		else if (est == 2 && bits [i] == '1')
 		{
-
-			estado = estado;
-
+			est = est;	
 		}
-
-		else if ( estado == 2 && bits [i] == '0')
-
+		else if (est == 2 && bits [i] == '0')
 		{
-
-			estado = 1;
-
+			est = 1;	
 		}
-
-		else if (estado == 1 && bits [i] == '1')
-
+		else if (est == 1 && bits [i] == '1')
 		{
-
-			estado = 0;
-
+			est = 0;	
 		}
-
 		else
-
 		{
-
-			estado = estado;
-
+			est = est;
 		}
-
-		i ++ ;
-
+		i++;
+	
 	}
-
-	if (estado == 0){
-
-		printf("\nEh multiplo de 3. ");
-
-	}	
-
-	if(estado !=0){
-
-		printf("\nNao eh multiplo de 3. ");
-
+	if (est == 0)
+	{
+		printf("\nE multiplo de 3. \n\n");
 	}
-
-	printf("\nA sequencia original eh %s" , bits);
-
-	printf("\nO estado eh igual %d", estado);
-
-
+		if (est != 0)
+	{
+		printf("\nNao e multiplo de 3. \n\n");
+	}
+	printf("\nA sequencia original e: %s\n\n", bits);
+	printf("\nO estado e igual a: %d\n\n", est);
+	
 	return 0;
-
 }
