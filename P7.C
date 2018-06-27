1257a28544cdfddf58ec17c242e502ca58ac1237
MirandaDiego <37850304+MirandaDiego@users.noreply.github.com>
2018-06-27 20:40:27 -0300

    Update P7.C

--- P7.C
+++ P7.C
@@ -1,35 +1,37 @@
-# include<stdio.h>
+//Diego Miranda Duarte 11711EAU019
 
-int  A ( int m, int n)
+#include <stdio.h>
+
+int A(int m, int n)
 
 {
 
-    if (m == 0 )
+    if (m==0)
 
-        return n + 1 ;
+        return n+1;
 
-    else  if (m> 0 && n == 0 )
+    else if (m>0 && n==0)
 
-        return  A (m - 1 , 1 );
+        return A(m-1, 1);
 
-    else  if (m> 0 && n> 0 )
+    else if (m>0 && n>0)
 
-        retorno  A (m - 1 , A (m, n - 1 ));
+        return A(m-1, A(m, n-1));
 
 }
 
-int  main ()
+int main ()
 
 {
 
-	int n = 0 , m = 0 ;
+	int n=0, m=0;
 
-	printf ( " Valores para nem: \ n " );
+	printf("Valores para n e m:\n");
 
-	scanf ( " % d% d " , & m, & n);
+	scanf("%d %d", &m, &n);
 
-	printf ( " % d " , A (m, n));
+	printf("%d", A(m, n));
 
-	return  0 ;
+	return 0;
 
 }
